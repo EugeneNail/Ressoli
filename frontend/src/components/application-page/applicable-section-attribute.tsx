a1afdd4f64fd3a0fa67e37e769f007f7832af96e
EugeneNail <eugene.a.nail@gmail.com>
2023-12-27 22:48:23 +0300

    Redraw ApplicableSectionAttribute
    Adapt it to boolean values

--- frontend/src/components/application-page/applicable-section-attribute.tsx
+++ frontend/src/components/application-page/applicable-section-attribute.tsx
@@ -3,15 +3,18 @@ import { Icon } from "../icon/icon";
 type ApplicableSectionAttributeProps = {
   icon: string;
   label: string;
-  value: string | number;
+  value?: string | number;
 };
 
 export function ApplicableSectionAttribute({ icon, label, value }: ApplicableSectionAttributeProps) {
   return (
     <div className="applicable-section-attribute">
       <Icon className="applicable-section-attribute__icon" name={icon} />
-      <p className="applicable-section-attribute__label">{label}</p>
-      <p className="applicable-section-attribute__value">{value}</p>
+      <p className="applicable-section-attribute__label" style={{ gridRow: value ? "1 / 2" : "1 / -1" }}>
+        {label}
+      </p>
+
+      {value && <p className="applicable-section-attribute__value">{value}</p>}
     </div>
   );
 }
